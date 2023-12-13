857c73641f4f62a5231fc425bdb2e6f849ea7ada
Krissny <110375406+Krissny@users.noreply.github.com>
2023-12-13 14:52:34 +0530

    Simplify code

--- script.js
+++ script.js
@@ -1,35 +1,45 @@
 const div = document.querySelector('.container');
-let prev = 256;
+let prev = 256;  // starting grid size 16 * 16
+
+//initial grid creation
 for(let i=0 ; i<256; i++){
     const divChild = document.createElement('div');
-    divChild.classList.add('children');
+    divChild.classList.add('children30');
     div.appendChild(divChild);
 }
+
 const btn = document.querySelectorAll('.btn');
-btn.forEach((e)=> { e.addEventListener('click', ()=>{
-    let newGrid = +( e.textContent[0] + e.textContent[1]);
-    console.log(newGrid);
+btn.forEach((e)=> { e.addEventListener('click', (e)=>{
+    let newGrid = +( e.currentTarget.textContent[0] + e.currentTarget.textContent[1]);
     if(newGrid != Math.sqrt(prev)){
         for(let i=0; i<prev; i++){
             div.removeChild(div.lastChild);
         }
-        for(i =0; i<newGrid*newGrid; i++){
+        for(let i =0; i<newGrid*newGrid; i++){
             const divChild = document.createElement('div');
-            divChild.classList.add('children');
             if(newGrid== 32){
-                divChild.style.width = '15px';
-                divChild.style.height = '15px';
+                divChild.classList.add('children15');
             }
             else if(newGrid ==48){
-                divChild.style.width = '10px';
-                divChild.style.height = '10px';
+                divChild.classList.add('children10');
             }
             else{
-                divChild.style.width = '30px';
-                divChild.style.height = '30px';
+                divChild.classList.add('children30');
             }
             div.appendChild(divChild);
         }
-        prev = newGrid*newGrid;
+        
     }
-})})
+    prev = newGrid*newGrid;
+})});
+const children30 = document.querySelectorAll('.children30');
+const children15 = document.querySelectorAll('.children15');
+
+children30.forEach((e)=>{e.addEventListener('mousedown',(e)=>{
+    console.log(e.currentTarget);
+    e.currentTarget.classList.add('black');
+} )})
+children15.forEach((e)=>{e.addEventListener('mousedown',(e)=>{
+    console.log(e.currentTarget);
+    e.currentTarget.classList.add('black');
+} )})
